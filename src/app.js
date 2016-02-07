6dd57876d44779fd1f4c04ea714a50d81b4d44ce
Dave Kelly <dmk3534@gmail.com>
2016-02-07 17:24:22 -0500

    Interface with Dash button

---
+++ src/app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var hwAddr = process.env.HW_ADDR;
+
+if (!hwAddr) {
+	console.log('Specify the hardware address of your Dash Button with HW_ADDR env variable');
+	process.exit(0);
+}
+
+var dashButton = require('node-dash-button');
+var dash;
+
+try {
+	dash = dashButton(hwAddr);
+} catch(err) {
+	console.log(err.message);
+	process.exit(0);
+}
+
+dash.on('detected', function() {
+	console.log('Button press detected');
+});
+
+console.log('\nListening for device button press...');
